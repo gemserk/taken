9fe814f3136bd623a33af7401544e958c290e483
Ariel Coppes <ariel.coppes@gemserk.com>
2011-04-30 14:16:59 -0300

    added background layer with backround

--- taken-core/src/main/java/com/gemserk/games/taken/GameScreen.java
+++ taken-core/src/main/java/com/gemserk/games/taken/GameScreen.java
@@ -1,18 +1,33 @@
 package com.gemserk.games.taken;
 
+import java.util.ArrayList;
+
 import javax.vecmath.Vector2f;
 
+import org.w3c.dom.Element;
+
+import com.artemis.Entity;
 import com.artemis.World;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.gemserk.commons.artemis.WorldWrapper;
+import com.gemserk.commons.artemis.components.SpatialComponent;
+import com.gemserk.commons.artemis.components.SpriteComponent;
+import com.gemserk.commons.artemis.systems.RenderLayer;
+import com.gemserk.commons.artemis.systems.SpriteRendererSystem;
+import com.gemserk.commons.artemis.systems.SpriteUpdateSystem;
 import com.gemserk.commons.gdx.ScreenAdapter;
 import com.gemserk.commons.gdx.box2d.Box2DCustomDebugRenderer;
 import com.gemserk.commons.gdx.camera.Camera;
+import com.gemserk.commons.gdx.camera.Libgdx2dCamera;
 import com.gemserk.commons.gdx.camera.Libgdx2dCameraTransformImpl;
+import com.gemserk.commons.gdx.resources.LibgdxResourceBuilder;
 import com.gemserk.commons.svg.inkscape.SvgDocument;
 import com.gemserk.commons.svg.inkscape.SvgDocumentHandler;
 import com.gemserk.commons.svg.inkscape.SvgInkscapeGroup;
@@ -22,6 +37,9 @@ import com.gemserk.commons.svg.inkscape.SvgInkscapePathHandler;
 import com.gemserk.commons.svg.inkscape.SvgParser;
 import com.gemserk.componentsengine.input.InputDevicesMonitorImpl;
 import com.gemserk.componentsengine.input.LibgdxInputMappingBuilder;
+import com.gemserk.resources.Resource;
+import com.gemserk.resources.ResourceManager;
+import com.gemserk.resources.ResourceManagerImpl;
 
 public class GameScreen extends ScreenAdapter {
 
@@ -32,6 +50,10 @@ public class GameScreen extends ScreenAdapter {
 	private InputDevicesMonitorImpl<String> inputDevicesMonitor;
 	
 	private Libgdx2dCameraTransformImpl camera = new Libgdx2dCameraTransformImpl();
+	
+	private Libgdx2dCamera backgroundLayerCamera = new Libgdx2dCameraTransformImpl();
+	
+	private Libgdx2dCamera hudLayerCamera = new Libgdx2dCameraTransformImpl();
 	
 	private Box2DCustomDebugRenderer box2dCustomDebugRenderer;
 
@@ -47,6 +69,8 @@ public class GameScreen extends ScreenAdapter {
 
 	private Camera cameraData;
 
+	private ResourceManager<String> resourceManager;
+
 	public GameScreen(Game game) {
 		this.game = game;
 		
@@ -61,7 +85,23 @@ public class GameScreen extends ScreenAdapter {
 
 		// create the scene...
 
-		worldWrapper = new WorldWrapper(new World());
+		World world = new World();
+		
+		worldWrapper = new WorldWrapper(world);
+		
+		ArrayList<RenderLayer> renderLayers = new ArrayList<RenderLayer>();
+		
+		// background layer
+		renderLayers.add(new RenderLayer(-1000, -100, backgroundLayerCamera));
+		// world layer
+		renderLayers.add(new RenderLayer(-100, 100, camera));
+		// hud layer
+		renderLayers.add(new RenderLayer(100, 1000, hudLayerCamera));
+		
+		worldWrapper.add(new SpriteUpdateSystem());
+		worldWrapper.add(new SpriteRendererSystem(renderLayers));
+		
+		worldWrapper.init();
 
 		physicsWorld = new com.badlogic.gdx.physics.box2d.World(new Vector2(0, -10f), false);
 		
@@ -70,7 +110,9 @@ public class GameScreen extends ScreenAdapter {
 		box2dCustomDebugRenderer = new Box2DCustomDebugRenderer(camera, physicsWorld);
 
 		inputDevicesMonitor = new InputDevicesMonitorImpl<String>();
-
+		
+		resourceManager = new ResourceManagerImpl<String>();
+		
 		new LibgdxInputMappingBuilder<String>(inputDevicesMonitor, Gdx.input) {
 			{
 				monitorKey("debug", Keys.D);
@@ -79,16 +121,28 @@ public class GameScreen extends ScreenAdapter {
 		
 		// load scene!!
 		
+		loadResources();
+		
+		Resource<Texture> resource = resourceManager.get("Background");
+		Sprite backgroundSprite = new Sprite(resource.get());
+		
+		Entity background = world.createEntity();
+		
+		background.addComponent(new SpatialComponent(new Vector2(0,0), new Vector2(viewportWidth, viewportWidth), 0f));
+		background.addComponent(new SpriteComponent(backgroundSprite, -101, new Vector2(0f, 0f), new Color(Color.WHITE)));
+		
+		background.refresh();
+		
 		SvgParser svgParser = new SvgParser();
 		svgParser.addHandler(new SvgDocumentHandler() {
 			@Override
-			protected void handle(SvgParser svgParser, SvgDocument svgDocument) {
+			protected void handle(SvgParser svgParser, SvgDocument svgDocument, Element element) {
 				GameScreen.this.svgDocument = svgDocument;
 			}
 		});
 		svgParser.addHandler(new SvgInkscapeGroupHandler() {
 			@Override
-			protected void handle(SvgParser svgParser, SvgInkscapeGroup svgInkscapeGroup) {
+			protected void handle(SvgParser svgParser, SvgInkscapeGroup svgInkscapeGroup, Element element) {
 				
 				if (svgInkscapeGroup.getGroupMode().equals("layer") && !svgInkscapeGroup.getLabel().equalsIgnoreCase("Physics")) {
 					svgParser.processChildren(false);
@@ -99,7 +153,9 @@ public class GameScreen extends ScreenAdapter {
 		});
 		svgParser.addHandler(new SvgInkscapePathHandler() {
 			@Override
-			protected void handle(SvgParser svgParser, SvgInkscapePath svgPath) {
+			protected void handle(SvgParser svgParser, SvgInkscapePath svgPath, Element element) {
+				
+				// String collisionMaskValue = element.getAttribute("collisionMask");
 				
 				Vector2f[] points = svgPath.getPoints();
 				Vector2[] vertices = new Vector2[points.length];
@@ -128,7 +184,7 @@ public class GameScreen extends ScreenAdapter {
 		camera.zoom(cameraData.zoom);
 		camera.move(cameraPosition.x, cameraPosition.y);
 		camera.rotate(cameraData.angle);
-
+		
 		worldWrapper.update((int) (delta * 1000f));
 		
 		inputDevicesMonitor.update();
@@ -154,6 +210,12 @@ public class GameScreen extends ScreenAdapter {
 	}
 
 	protected void loadResources() {
+		
+		new LibgdxResourceBuilder(resourceManager) {
+			{
+				texture("Background", "data/background-512x512.jpg");
+			}
+		};
 
 	}
 
